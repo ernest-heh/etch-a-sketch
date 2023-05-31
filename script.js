5d45ff48028212873aba313285dab553e3b19bbe
Ernest Ho <ernest.heh@gmail.com>
2023-05-31 16:20:43 +0800

    add input to draw grid with given value

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 const grid = document.querySelector(".grid");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
-function drawGrid(rows, cols) {
-  grid.style.setProperty("--grid-rows", rows);
-  grid.style.setProperty("--grid-cols", cols);
-  for (c = 0; c < rows * cols; c++) {
+function clearGrid() {
+  grid.innerHTML = "";
+}
+
+function drawGrid(size) {
+  if (size < 2 || size > 100) {
+    alert("Pick a number between 2 and 100");
+    return;
+  }
+
+  grid.style["grid-template-columns"] = `repeat(${size}, 1fr)`;
+  grid.style["grid-template-rows"] = `repeat(${size}, 1fr)`;
+
+  for (c = 0; c < size * size; c++) {
     let cell = document.createElement("div");
     grid.appendChild(cell).className = "grid-item";
   }
 }
 
-drawGrid(32, 32);
+drawGrid(24);
 
 const squares = document.querySelectorAll(".grid-item");
 squares.forEach((square) => {
   square.addEventListener("mouseover", () => {
     square.style["background-color"] = "grey";
   });
+});
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  clearGrid();
+  drawGrid(input.value);
 });
